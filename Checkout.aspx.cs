96b639f7372a4e9745202b5492ac0a639b1a64c0
Jamunatharan <39282535+Jamunatharan@users.noreply.github.com>
2019-01-24 12:07:32 +0000

    Update Checkout.aspx.cs

--- Checkout.aspx.cs
+++ Checkout.aspx.cs
@@ -10,9 +10,7 @@ using System.Configuration;
 
 public partial class Checkout : System.Web.UI.Page
 {
-
-    //SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RestaurantConnectionString"].ConnectionString);
-    SqlConnection conn = new SqlConnection(@"Data Source=restaurantkaran.database.windows.net;Initial Catalog=Restaurant;User ID=restaurantkaran;Password=musebh9H;");
+SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DB_A41AE2_Restaurant"].ConnectionString);
 
     protected void Page_Load(object sender, EventArgs e)
     {
